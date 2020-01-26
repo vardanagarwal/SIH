b5b92ce6b8613f82492cfa77a35b26f84b1c1437
vardanagarwal <vardan.179301222@muj.manipal.edu>
2020-01-26 17:30:49 +0530

    Fixed minor bug

--- generate_sky_only.py
+++ generate_sky_only.py
@@ -9,20 +9,18 @@ import os
 import cv2
 
 pth = 'C:\\Users\\hp\\Documents\\SIH\\dataset'
-done = ['10066', '10870', '10917', '1093', '11160', '11331', '162', '17218', '17244', '18590', '19106' ,'19306', '19388']
 folders = os.listdir(pth)
 for folder in folders:
-    if folder not in done:
-        dst = os.path.join(pth, folder, "sky")
-        os.makedirs(dst)
-        src = os.path.join(pth, folder, folder)
-        images = os.listdir(src)
-        mask_path = os.path.join(pth, folder, folder)+".png"
-        mask = cv2.imread(mask_path, 0)
-        for image in images:
-            img = cv2.imread(src+'\\'+image)
-            if mask.shape != img.shape:
-                img = cv2.resize(img, (mask.shape[1], mask.shape[0]), interpolation = cv2.INTER_LINEAR)
-                cv2.imwrite(src+'\\'+image, img)
-            res = cv2.bitwise_and(img, img, mask=mask)
-            cv2.imwrite(dst+'\\'+image, res)
+    dst = os.path.join(pth, folder, "sky")
+    os.makedirs(dst)
+    src = os.path.join(pth, folder, folder)
+    images = os.listdir(src)
+    mask_path = os.path.join(pth, folder, folder)+".png"
+    mask = cv2.imread(mask_path, 0)
+    for image in images:
+        img = cv2.imread(src+'\\'+image)
+        if mask.shape != img.shape:
+            img = cv2.resize(img, (mask.shape[1], mask.shape[0]), interpolation = cv2.INTER_LINEAR)
+            cv2.imwrite(src+'\\'+image, img)
+        res = cv2.bitwise_and(img, img, mask=mask)
+        cv2.imwrite(dst+'\\'+image, res)
